5bd9b984eb9567f94c3330c1a54a96d03a30d875
Stefan Ledin <stefan.ledin@gmail.com>
2014-05-09 23:44:51 +0200

    lagt till olika config-filer baserat på environment

---
+++ config/local.php
@@ -0,0 +1,13 @@
+<?php
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define('DB_NAME', 'stefanledin_se');
+
+/** MySQL database username */
+define('DB_USER', 'root');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'root');
+
+/** MySQL hostname */
+define('DB_HOST', 'localhost');
--- wp-config.php
+++ wp-config.php
@@ -2,18 +2,12 @@
 define( 'WP_CONTENT_DIR', dirname( __FILE__ ) . '/content' );
 define( 'WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/content' );
 
-// ** MySQL settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
-define('DB_NAME', 'stefanledin_se');
-
-/** MySQL database username */
-define('DB_USER', 'root');
-
-/** MySQL database password */
-define('DB_PASSWORD', 'root');
-
-/** MySQL hostname */
-define('DB_HOST', 'localhost');
+if ($_SERVER['HTTP_POST'] == 'HTTP_HOST') {
+	define('ENVIRONMENT', 'local');
+} else {
+	define('ENVIRONMENT', 'production');
+}
+require 'config/'.ENVIRONMENT.'.php';
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
