1fb7d54802a2ded5e722a4f45838378279ff674c
Stefan Ledin <stefan.ledin@gmail.com>
2014-05-11 14:51:51 +0200

    lagt till paginering

--- content/themes/stefanledin_se/loop-post.php
+++ content/themes/stefanledin_se/loop-post.php
@@ -29,4 +29,8 @@
 			</div>
 		</article>
 	<?php endwhile; ?>
+	<ul class="pager">
+		<li class="previous"><?php previous_posts_link(); ?></li>
+		<li class="next"><?php next_posts_link(); ?></li>
+	</ul>
 <?php endif; ?>
